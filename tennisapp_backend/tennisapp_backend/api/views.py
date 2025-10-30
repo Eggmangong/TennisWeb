b20bfe68a7092029b8507fbf1afd2648404dfe22
Egg Man <125430646+Eggmangong@users.noreply.github.com>
2025-10-30 12:44:59 -0700

    Add backend project tennisapp_backend

---
+++ tennisapp_backend/tennisapp_backend/api/views.py
@@ -0,0 +1,36 @@
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, RegisterSerializer, ProfileUpdateSerializer
+from .models import Profile
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class ProfileUpdateView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        user = request.user
+        profile, _ = Profile.objects.get_or_create(user=user)
+        serializer = ProfileUpdateSerializer(profile, data=request.data, partial=True, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, *args, **kwargs):
+        return self.put(request, *args, **kwargs)
